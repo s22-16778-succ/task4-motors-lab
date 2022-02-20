01e4d1e8c0a0933aeafc4d38c7abb8220d6976d5
Felipe Borja <fborja@vt.edu>
2022-02-20 16:50:48 -0500

    begin integration of servo code

--- integrated_motors_sketch/integrated_motors_sketch.ino
+++ integrated_motors_sketch/integrated_motors_sketch.ino
@@ -8,11 +8,20 @@
 *          Sahil Saini (sssaini)
 */
 
+#include <Servo.h>
+
+// Servo variables
+Servo myservo;  // create servo object to control a servo
+int potpin_angle = 0;// analog pin used to connect the potentiometer
+int val_angle;    // variable to read the value from the analog pin
+
 // Pin assignments
 
 void setup() {
   Serial.begin(9600);
   Serial.print("Hi :)");
+
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
@@ -21,17 +30,18 @@ void loop() {
     char c = Serial.read();
     flagSwitch(c);
   }
+  setServo();
 }
 
 void flagSwitch(char c) {
   switch (c) {
-    case 'd':
+    case 'D':
       driveDC();
       break;
-    case 'v':
+    case 'V':
       driveServo();
       break;
-    case 'p':
+    case 'P':
       driveStepper();
       break;
     default:
@@ -55,4 +65,16 @@ void driveStepper() {
   digitalWrite(3, HIGH);
   delay(1000);
   digitalWrite(3, LOW);
+}
+
+// Sahil's Servo Code
+
+void setServo() {
+  val_angle = analogRead(potpin_angle);
+  val_angle = map(val_angle, 0, 1023, 0, 180);
+  myservo.write(val_angle);
+  val_angle = map(val_angle, 0, 180, 0, 200);
+  Serial.write(val_angle);
+  delay(5);
+
 }
